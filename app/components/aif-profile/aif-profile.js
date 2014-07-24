54c7b2e2ccbd34b962d34a7fca7f4c13b7f55ef3
Luke D <lukedary@gmail.com>
2014-07-24 10:11:01 -0400

    Profile folder and files
    Basic structure copied from Rate page

---
+++ app/components/aif-profile/aif-profile.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+    
+    Polymer('aif-profile', {
+        starting: 0,
+        rating: 0,
+        
+        ready: function () {
+            this.addEventListener('something', function () {
+                console.log('in here');
+            });
+        },
+        
+        submit: function () {
+            var that = this;
+            
+            this.$.overlay.toggle();
+            
+            setTimeout(function () {
+                that.$.overlay.toggle();
+                
+                that.fire('transition', {
+                    page: 'main'
+                });
+            }, 2000);
+        },
+        
+        cancel: function () {
+            history.back();
+        }
+    });
+}());
